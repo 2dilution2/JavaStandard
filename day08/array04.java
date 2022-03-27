b34ca500a35db7e098ac9781efb3f519187bab97
Heeseok Lee <2dilution2@gmail.com>
2022-03-28 03:28:11 +0900

    Heeseok Lee

---
+++ day08/array04.java
@@ -0,0 +1,20 @@
+package day08;
+
+import java.util.Arrays;
+
+public class array04 {
+    // 배열의 활용, 섞기
+    public static void main(String[] args) {
+        int[] numArr = {0,1,2,3,4,5,6,7,8,9};
+        System.out.println(Arrays.toString(numArr));
+
+        for (int i = 0; i < 100; i++) {
+            int n = (int)(Math.random() * 10);
+            int tmp = numArr[0];
+            numArr[0] = numArr[n]; 
+            numArr[n] = tmp; 
+        }
+
+        System.out.println(Arrays.toString(numArr));
+    }
+}
