cfe9af10877086e501850f1ae619b5d7325d8ae7
Heeseok Lee <2dilution2@gmail.com>
2022-03-30 00:11:51 +0900

    HeeseokLee

--- day09/array09.java
+++ day09/array09.java
@@ -15,7 +15,7 @@ public class array09 {
         int kortotal = 0, engtotal = 0, mathtotal = 0;
 
         System.out.println("번호    국어    영어    수학    총점    평균");
-        System.out.println("==========================================");
+        System.out.println("==============================================");
 
         for (int i = 0; i < score.length; i++) {
             int sum = 0;        // 개인의 총점
@@ -24,18 +24,18 @@ public class array09 {
             kortotal += score[i][0];
             engtotal += score[i][1];
             mathtotal += score[i][2];
-            System.out.printf("%3d", i+1);
+            System.out.printf("%3d\t", i+1);
 
             for (int j = 0; j < score[i].length; j++) {
                 sum += score[i][j];
-                System.out.printf("%5d", score[i][j]);
+                System.out.printf("%5d\t", score[i][j]);
             }
 
             aver = sum /(float)score[i].length; // 평균 계산
-            System.out.printf("%5d %5.1f%n", sum, aver);
+            System.out.printf("%5d\t %5.1f%n", sum, aver);
         }
 
-        System.out.println("==========================================");   
+        System.out.println("==============================================");   
         System.out.printf("총점 : %3d %4d %4d%n", kortotal, engtotal, mathtotal); 
     }
 }
