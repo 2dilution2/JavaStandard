c970697f2b797cef672febdfd27c686e58359988
Heeseok Lee <2dilution2@gmail.com>
2022-04-01 23:48:05 +0900

    HeeseokLeee

--- day11/object04.java
+++ day11/object04.java
@@ -14,22 +14,21 @@ public class object04 {
         c2.kind = "Spade";
         c2.number = 4;
 
-        System.out.println("c1은 " + c1.kind + c1.number + "이며, 크기는 (" + c1.width + ", " + c1.height + ")");
-        System.out.println("c2은 " + c2.kind + c2.number + "이며, 크기는 (" + c2.width + ", " + c2.height + ")");
+        System.out.println("c1은 " + c1.kind + c1.number + "이며, 크기는 (" + Card.width + ", " + Card.height + ")");
+        System.out.println("c2은 " + c2.kind + c2.number + "이며, 크기는 (" + Card.width + ", " + Card.height + ")");
         System.out.println("c1의 weidth와 height를 각각 50, 80으로 변경 합닏다.");
 
         c1.width = 50;
         c1.height = 80;
 
-        System.out.println("c1은 " + c1.kind + c1.number + "이며, 크기는 (" + c1.width + ", " + c1.height + ")");
-        System.out.println("c2은 " + c2.kind + c2.number + "이며, 크기는 (" + c2.width + ", " + c2.height + ")");
+        System.out.println("c1은 " + c1.kind + c1.number + "이며, 크기는 (" + Card.width + ", " + Card.height + ")");
+        System.out.println("c2은 " + c2.kind + c2.number + "이며, 크기는 (" + Card.width + ", " + Card.height + ")");
     }
 }
 
-class Card {
-    String kind;                // 무늬
-    int number;                 // 숫자
-
-    static int width = 100;     // 폭
-    static int height = 250;    // 높이
+class Card{
+    String kind;
+    int number;
+    static int width = 100;
+    static int height = 250;
 }
---
+++ day12/object05.java
@@ -0,0 +1,9 @@
+package day12;
+
+public class object05 {
+    // 메서드의 구현부
+    int add (int x, int y){
+        int result = x + y;
+        return result;
+    }
+}
